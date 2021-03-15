808757267a277cfd766f331f97a03c844700d5dd
MijMa <36820079+MijMa@users.noreply.github.com>
2021-03-15 11:19:42 +0200

    Validator testit valmiita

--- test/validator.js
+++ test/validator.js
@@ -16,11 +16,27 @@ const requestBody = {
 	"currentdate": "\"2000-10-21T09:24:50.346Z\""
 }
 
+const requestBodyFail = {
+	"name": "optimal",
+	"age": "3",
+	"email": "kirov.ready@redarmy.ru",
+	"website": "https://www.set.ru",
+	"currentdate": "\"2000-10-21T09:24:50.346Z\""
+}
+
 describe("validator validate()", () => {
-    it("should respond with status 200", async function() {
+
+    it("should respond with status 200, if 'id' data field is present", async function() {
 		
 		const response = await chai.request(server).put("/users").send(requestBody);
 		expect(response.status).to.equal(200);
         //response.should.have.status(200);
+    })
+
+	it("should respond with status 400, if 'id' data field is missing", async function() {
+
+		const response = await chai.request(server).put("/users").send(requestBodyFail);
+		expect(requestBodyFail.id).to.not.exist;
+		expect(response.status).to.equal(400);
     })
 })
